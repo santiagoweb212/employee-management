1b0d134af9346297c79df871e6769625cd95369a
santiagoweb212 <juanusca45@gmail.com>
2023-09-03 11:29:54 -0500

    cargar datos mockup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { MockupDataUploadService } from './modules/mockup-data-upload/mockup-data-upload.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService);
-  console.log(config.get('DB_HOST'));
+  const mockupService = app.get(MockupDataUploadService);
+  await mockupService.createProyect();
+  await mockupService.createDepartaments();
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/mockup-data-upload/mockup-data-upload.service.ts
+++ src/modules/mockup-data-upload/mockup-data-upload.service.ts
@@ -9,7 +9,18 @@ import {
 } from './mockup.data';
 @Injectable()
 export class MockupDataUploadService {
-  createProyect() {
+  constructor(private readonly proyectModel: typeof Proyect) {}
+  async createProyect() {
     console.log('hola esty aca mockupDataUpload');
+    const newProyect = await this.proyectModel.bulkCreate(mockupProjects);
+    console.log(newProyect);
+  }
+  async createDepartaments() {
+    /* const newDepartaments = await Departament.bulkCreate(mockupDepartments);
+    console.log(newDepartaments); */
+  }
+  async createPositions() {
+    /* const newPositions = await Position.bulkCreate(mockupPositions);
+    console.log(newPositions); */
   }
 }
